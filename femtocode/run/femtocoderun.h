b3989b840a3d20622028ed4762468414cfcd313d
Jim Pivarski <jpivarski@gmail.com>
2016-12-17 15:45:33 -0600

    Thinking about the _sizeflat algorithm

--- femtocode/run/femtocoderun.h
+++ femtocode/run/femtocoderun.h
@@ -21,14 +21,36 @@ void plus_flatflat(int64_t len, IN1* in1array, IN2* in2array, OUT* outarray) {
 }
 
 // template <typename IN1, typename IN2, typename OUT>
-// void plus_sizeflat(int64_t size1len, int64_t* size1array,
+// void plus_sizeflat(int64_t size1len, int64_t size1depth, int64_t* size1array,
 //                    int64_t in1len, IN1* in1array,
-//                    int64_t in2len, IN1* in2array,
+//                    int64_t in2len, IN2* in2array,
 //                    OUT* outarray) {
-  
+//   int64_t size1i = 0;
+//   int64_t size1d = 0;
+//   int64_t in1i = 0;
+//   int64_t in2i = 0;
+//   int64_t size1countdowntop = 0;
+//   std::stack<int64_t> size1countdown;
+
+//   while (size1i != size1len) {
+//     if (size1countdowntop == 0) {
+//       size1countdowntop = size1array[size1i];
+//     }
+
+//     if (size1d == size1depth) {
+//       while (size1countdowntop != 0) {
+//         outarray[in1i] = in1array[in1i] + in2array[in2i];
+//         in1i++;
+//         size1countdowntop--;
+//       }
+//     }
+//     else {
+      
 
 
 
-//   for (int64_t i = 0;  i < len;  i++)
-//     outarray[i] = in1array[i] + in2array[i];
+//     }
+
+//     size1i++;
+//   }
 // }
