628d50a96ed090f7c1f39cf8ca2bd93ee15e29fb
Jim Pivarski <jpivarski@gmail.com>
2017-02-10 21:38:53 -0600

    the event loop scales appropriately

--- fromroot/femtocode/fromroot/_fastreader.cpp
+++ fromroot/femtocode/fromroot/_fastreader.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <iostream>
 #include <vector>
 #include <string>
 
@@ -105,10 +104,6 @@ public:
 };
 
 static PyObject* fillarrays(PyObject* self, PyObject* args) {
-  struct timeval startTime;
-
-  gettimeofday(&startTime, 0);
-
   char* fileName;
   char* treeName;
   PyObject* branches_arrays;
