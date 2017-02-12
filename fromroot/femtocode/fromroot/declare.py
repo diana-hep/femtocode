836432967ed479b03e995ce3bb5ecb6174fc3a3b
Jim Pivarski <jpivarski@gmail.com>
2017-02-11 19:57:50 -0600

    start defining dataset-declaration language

---
+++ fromroot/femtocode/fromroot/declare.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+# Copyright 2016 DIANA-HEP
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import glob
+import re
+try:
+    from urlparse import urlparse
+except ImportError:
+    from urllib.parse import urlparse
+
+import strictyaml
+import XRootD.client
+from XRootD.client.flags import OpenFlags
+
+def filesFromPath(path):
+    url = urlparse(path)
+    if url.scheme is not None:
+        fs = XRootD.client.FileSystem(url.scheme + "://" + url.netloc)
+
+        def exists(path):
+            return fs.locate(path, OpenFlags.NONE)[0].ok
+
+        def search(path):
+            status, listing = fs.dirlist(path)
+            if status.ok:
+                for x in listing.dirlist:
+                    for y in search(path.rstrip("/") + "/" + x.name):
+                        yield y
+            elif exists(path):
+                yield path
+
+        m = re.match(r"(^[^\*\[\]\{\}]*/)*", url.path)
+        if m is None:
+            path = url.path
+        else:
+            path = m.group(1)
+
+        for x in search(path):
+            if glob.fnmatch.fnmatchcase(x, url.path):
+                yield url.scheme + "://" + url.netloc + x
+
+    else:
+        for x in glob.glob(url.path):
+            yield x
+
+
+
+
+# boolean
+# integer: min max
+# real: min max
+# extended: min max
+# string: charset (only bytes), fewest, most
+
+# collection: items, fewest, most, ordered
+# vector: items, dimensions
+# matrix: items, dimensions
+# tensor: items, dimensions
+# record: fields
+
+
+
+
+
+
+# def _filesFromPath(path):
+#     # FIXME: move this to a C function to remove dependence on PyROOT and therefore Python 2
+#     import ROOT
+#     for x in ROOT.TSystemDirectory(path, path).GetListOfFiles():
+#         yield x.GetName()
+
+# def filteredFilesFromPath(path, filter):
+#     for x in _filesFromPath(path):
+#         if glob.fnmatch.fnmatch(x, filter):
+#             yield path.rstrip("/") + "/" + x
---
+++ fromroot/tests/test_declare.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+# Copyright 2016 DIANA-HEP
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+import unittest
+
+import numpy
+
+import femtocode.fromroot.declare as declare
+
+class TestDeclare(unittest.TestCase):
+    def runTest(self):
+        pass
+
+    def test_config(self):
+        definition = """
+sources:
+  - MuOnia-2016-CF-23Sep2016-v1:
+      format: root
+      paths:
+        - root://cmseos.fnal.gov//store/data/Run2016{C,F}/MuOnia/AOD/23Sep2016-v1/*/*.root
+
+  - MuOnia-2016-CF-PromptReco-v2:
+      format: root
+      paths:
+        - root://cmseos.fnal.gov//store/data/Run2016{C,F}/MuOnia/AOD/PromptReco-v2/*/*.root
+
+dataset:
+  name: MuOnia
+
+  sources:
+    - MuOnia-2016C
+    - MuOnia-2016F
+
+  schema:
+
+    muons:
+      type: collection
+      items:
+        type: record
+        fields:
+          pt:
+            type: real
+            min: 0
+            max: almost(inf)
+            source:
+              data: patMuons_slimmedMuons__PAT.obj.m_state.p4Polar_.fCoordinates.fPt
+              size: patMuons_slimmedMuons__PAT.obj
+              dtype: float64
+          eta:
+            type: double
+            source:
+              data: patMuons_slimmedMuons__PAT.obj.m_state.p4Polar_.fCoordinates.fEta
+              size: patMuons_slimmedMuons__PAT.obj
+              dtype: float64
+          phi:
+            type: double
+            min: -pi
+            max: pi
+            source:
+              data: patMuons_slimmedMuons__PAT.obj.m_state.p4Polar_.fCoordinates.fPhi
+              size: patMuons_slimmedMuons__PAT.obj
+              dtype: float64
+
+    jets:
+      type: collection
+      items:
+        type: record
+        fields:
+          pt:
+            type: real
+            min: 0
+            max: almost(inf)
+            source:
+              data: patJets_slimmedJets__PAT.obj.m_state.p4Polar_.fCoordinates.fPt
+              size: patJets_slimmedJets__PAT.obj
+              dtype: float64
+          eta:
+            type: double
+            source:
+              data: patJets_slimmedJets__PAT.obj.m_state.p4Polar_.fCoordinates.fEta
+              size: patJets_slimmedJets__PAT.obj
+              dtype: float64
+          phi:
+            type: double
+            min: -pi
+            max: pi
+            source:
+              data: patJets_slimmedJets__PAT.obj.m_state.p4Polar_.fCoordinates.fPhi
+              size: patJets_slimmedJets__PAT.obj
+              dtype: float64
+"""
