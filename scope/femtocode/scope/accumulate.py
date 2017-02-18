89849c2b5fe1544c288e231c2c567f2560d85eea
Jim Pivarski <jpivarski@gmail.com>
2017-02-18 12:27:32 -0600

    have to think

--- scope/femtocode/scope/accumulate.py
+++ scope/femtocode/scope/accumulate.py
@@ -33,14 +33,57 @@ import time
 tallyman = sys.argv[1]
 minions = sys.argv[2:]
 
-gabos = context.socket(zmq.REP)
-gabos.bind("tcp://*:5556")
-
-queryBroadcast = Broadcast("5557")
-
 startTime = time.time()
 def now():
     return time.time() - startTime
+
+# class Foreman(threading.Thread):
+#     delay = 1.0
+#     deadThreshold = 10
+
+#     def __init__(self):
+#         super(Foreman, self).__init__()
+#         self.lastMessage = {}
+#         self.lastMessageLock = threading.Lock()
+#         self.newQueries = queue.Queue()
+#         self.activeQueries = {}
+#         self.daemon = True
+
+#     def update(self, minion):
+#         # can be called from other threads
+#         with self.lastMessageLock:
+#             self.lastMessage[minion] = now()
+
+#     def run(self):
+#         while True:
+#             with self.lastMessageLock:
+#                 for minion, lastMessage in list(self.lastMessage.items()):
+#                     if now() > lastMessage + (self.delay * self.deadThreshold):
+#                         print("{} is dead".format(minion))
+#                         del self.lastMessage[minion]
+
+#             queries = drainQueue(self.newQueries)
+
+#             if len(queries) > 0:
+#                 with self.lastMessageLock:
+#                     survivors = sorted(self.lastMessage)
+
+#                 for query in queries:
+#                     responsibilities, unassigned = assign(0, query.numGroups, minions, survivors)
+#                     slack = assignExtra(0, query.numGroups, unassigned, survivors)
+#                     assignments = dict((minion, [resp] + slack[minion]) for minion, resp in responsibilities.items())
+#                     todo = set(range(query.numGroups))
+
+#                     self.activeQueries[query.queryid] = ActiveQuery(query, assignments, todo)
+
+#             time.sleep(self.delay)
+
+# foreman = Foreman()
+# foreman.start()
+
+queryBroadcast = Broadcast("5557")
+
+print("tallyman {} starting".format(tallyman))
 
 class ActiveQuery(Message):
     __slots__ = ("query", "assignments", "todo")
@@ -56,64 +99,61 @@ class Foreman(threading.Thread):
     def __init__(self):
         super(Foreman, self).__init__()
         self.lastMessage = {}
-        self.lastMessageLock = threading.Lock()
+        self.gabos = context.socket(zmq.REP)
+        self.gabos.bind("tcp://*:5556")
+        self.gabos.RCVTIMEO = int(self.delay * self.deadThreshold * 1000)
+
         self.newQueries = queue.Queue()
-        self.activeQueries = {}
+
         self.daemon = True
 
-    def update(self, minion):
-        # can be called from other threads
-        with self.lastMessageLock:
-            self.lastMessage[minion] = now()
+    def ping(self, minion):
+        self.lastMessage[minion] = now()
+
+    def updateMinions(self):
+        for minion, lastMessage in self.lastMessage.items():
+            if now() > lastMessage + (self.delay * self.deadThreshold):
+                print("{} is dead".format(minion))
+                del self.lastMessage[minion]
+        
+    def survivors(self):
+        self.updateMinions()
+        return sorted(self.lastMessage)
 
     def run(self):
         while True:
-            with self.lastMessageLock:
-                for minion, lastMessage in list(self.lastMessage.items()):
-                    if now() > lastMessage + (self.delay * self.deadThreshold):
-                        print("{} is dead".format(minion))
-                        del self.lastMessage[minion]
+            try:
+                message = self.gabos.recv_pyobj()
+            except zmq.Again:
+                pass
 
-            queries = drainQueue(self.newQueries)
+            else:
+                if isinstance(message, GiveMeWork):
+                    if message.minion in minions:
+                        assert message.tallyman == tallyman
+                        self.ping(message.minion)
+                        self.gabos.send_pyobj(HeresSomeWork(tallyman, message.queryid, [1, 2, 3]))
+                    else:
+                        self.gabos.send(b"")
 
-            if len(queries) > 0:
-                with self.lastMessageLock:
-                    survivors = sorted(self.lastMessage)
+                elif isinstance(message, Heartbeat):
+                    if message.identity in minions:
+                        self.ping(message.identity)
+                        self.gabos.send_pyobj(Heartbeat(tallyman))
+                    else:
+                        self.gabos.send(b"")
 
-                for query in queries:
-                    responsibilities, unassigned = assign(0, query.numGroups, minions, survivors)
-                    slack = assignExtra(0, query.numGroups, unassigned, survivors)
-                    assignments = dict((minion, [resp] + slack[minion]) for minion, resp in responsibilities.items())
-                    todo = set(range(query.numGroups))
-
-                    self.activeQueries[query.queryid] = ActiveQuery(query, assignments, todo)
-
-            time.sleep(self.delay)
+            self.updateMinions()
 
 foreman = Foreman()
 foreman.start()
 
-print("tallyman {} starting".format(tallyman))
-
 time.sleep(1)
 query = CompiledQuery(tallyman, 1, 100)
-foreman.newQueries.put(query)
 queryBroadcast.send(query)
 
-while True:
-    message = gabos.recv_pyobj()
-    print(message)
-    if isinstance(message, GiveMeWork):
-        if message.minion in minions:
-            assert message.tallyman == tallyman
-            foreman.update(message.minion)
-            gabos.send_pyobj(HeresSomeWork(tallyman, message.queryid, [1, 2, 3]))
-        else:
-            gabos.send(b"")
+time.sleep(5)
+query = CompiledQuery(tallyman, 2, 100)
+queryBroadcast.send(query)
 
-    elif isinstance(message, Heartbeat):
-        if message.identity in minions:
-            foreman.update(message.identity)
-            gabos.send_pyobj(Heartbeat(tallyman))
-        else:
-            gabos.send(b"")
+time.sleep(100)
