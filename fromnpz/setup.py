ee8c00126e894960957812ace6a383e33083059f
Jim Pivarski <jpivarski@gmail.com>
2017-02-24 14:47:43 -0600

    get the one file with useful stuff from the other repo

--- fromnpz/setup.py
+++ fromnpz/setup.py
@@ -33,7 +33,7 @@ setup(name = "femtocode-fromnpz",
       download_url = "https://github.com/diana-hep/femtocode",
       license = "Apache Software License v2",
       test_suite = "tests",
-      install_requires = ["femtocode"],
+      install_requires = ["femtocode", "femtocode-run", "ruamel.yaml", "numpy"],
       tests_require = [],
       classifiers = ["Development Status :: 2 - Pre-Alpha",
                      # "Development Status :: 5 - Production/Stable",   # no way!
