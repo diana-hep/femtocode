4fcace5057dbee98e3311f94b5263ececf6577ce
Jim Pivarski <jpivarski@gmail.com>
2017-03-13 16:56:54 -0500

    disk rollover implemented but not tested

--- server/femtocode/server/tallycache.py
+++ server/femtocode/server/tallycache.py
@@ -76,11 +76,12 @@ class PartitionedCacheOccupant(object):
         raise NotImplementedError
 
 class RolloverCache(object):
-    def __init__(self, directory, memoryMarginBytes, diskMarginBytes, rolloverTime):
+    def __init__(self, directory, memoryMarginBytes, diskMarginBytes, rolloverTime=24*60*60, idchars=4):
         self.directory = "/tmp/downloads/cache"
         self.memoryMarginBytes = memoryMarginBytes
         self.diskMarginBytes = diskMarginBytes
         self.rolloverTime = rolloverTime
+        self.idchars = idchars
 
         assert os.path.exists(self.directory) and os.path.isdir(self.directory)
 
@@ -88,19 +89,27 @@ class RolloverCache(object):
         self.order = deque()
         self.lookup = {}
 
-        self.updateRollovers()
+    def partialdir(self, when):
+        return str(int(math.floor(when / self.rolloverTime)))
 
-    def currentDir(self):
-        return str(int(math.floor(time.time() / self.rolloverTime)))
+    def fulldir(self, when):
+        return os.path.join(self.directory, self.partialdir(when))
 
-    def updateRollovers(self):
-        self.rollovers = os.listdir(self.directory)
-        if currentDir not in self.rollovers:
-            os.mkdir(os.path.join(self.directory, currentDir))
-            self.rollovers = os.listdir(self.directory)
-            assert currentDir in self.rollovers
+    def fullpath(self, fulldir, queryid):
+        return reduce(os.path.join, [fulldir] + [queryid[start:start + self.idchars] for start in range(0, len(queryid), self.idchars)]) + ".pkl"
 
-        self.rollovers.sort(key=lambda x: -int(x))
+    def fullpaths(self, when, queryid):
+        return [self.fullpath(fulldir, queryid) for fulldir in self.rollovers(when)]
+            
+    def rollovers(self, when):
+        rollovers = os.listdir(self.directory)
+        if self.partialdir(when) not in rollovers:
+            os.mkdir(self.fulldir(when))
+            rollovers = os.listdir(self.directory)
+            assert self.partialdir(when) in rollovers
+
+        rollovers.sort(key=lambda x: -int(x))
+        return [os.path.join(self.directory, partialdir) for partialdir in rollovers]
 
     def __contains__(self, query):
         if isinstance(query, (int, long)):
@@ -134,5 +143,65 @@ class RolloverCache(object):
             if self.queryids[occupant.query.id] == 0:
                 del self.queryids[occupant.query.id]
 
-            occupant.todisk()
+            self.todisk(occupant)
 
+    def rolloverDisk(self):
+        while psutil.disk_usage(self.directory).free < self.diskMarginBytes:
+            rollovers = self.rollovers(time.time())
+            for dirpath, dirnames, filnames in os.walk(rollovers[-1], topdown=False):
+                for name in filenames:
+                    os.remove(os.path.join(dirpath, name))
+                for name in dirnames:
+                    os.rmdir(os.path.join(dirpath, name))
+
+    def todisk(self, occupant):
+        now = time.time()
+        self.rollovers(now)
+
+        if isinstance(occupant, SimpleCacheOccupant):
+            # each query contains *in principle* multiple queries because of (very unlikely) query.id collision
+            fullpath = self.fullpath(self.fulldir(now), occupant.query.id)
+            if os.path.exists(fullpath):
+                values = pickle.load(open(fullpath, "rb"))
+            else:
+                values = []
+
+            if occupant.query not in [x.query for x in values]:
+                values = values + [occupant]
+            
+            pickle.dump(values, open(fullpath, "wb"), pickle.HIGHEST_PROTOCOL)
+
+        else:
+            raise NotImplementedError
+
+    def fromdisk(self, query):
+        now = time.time()
+        for fullpath in self.fullpaths(now, query.id):
+            if os.path.exists(fullpath):
+                # each query contains *in principle* multiple queries because of (very unlikely) query.id collision
+                for occupant in pickle.load(open(fullpath, "rb")):
+                    if occupant.query == query:
+                        # if we found this in and old (not the most recent) directory
+                        if fullpath != self.fullpath(self.fulldir(now), query.id):
+                            self.todisk(occupant)   # put it in the most recent directory
+
+                        return occupant
+
+        assert False, "couldn't find {0} on disk".format(query.id)
+
+    def ondisk(self, query):
+        if isinstance(query, (int, long)):
+            queryid = query
+        else:
+            queryid = query.id
+
+        for fullpath in self.fullpaths(time.time(), queryid):
+            if os.path.exists(fullpath):
+                if isinstance(query, (int, long)):
+                    return True
+
+                for occupant in pickle.load(open(fullpath, "rb")):
+                    if occupant.query == query:
+                        return True
+
+        return False
