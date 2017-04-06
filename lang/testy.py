e152fd902c1834b159f33bb20a642a4a256794ec
Jim Pivarski <jpivarski@gmail.com>
2017-04-06 15:39:01 -0500

    testy.py helps me figure out the pattern

---
+++ lang/testy.py
@@ -0,0 +1,59 @@
+numEntries = 2
+data1 = [1, 2, 3, 4, 5, 6] * 2
+data2 = [1, 2, 3, 4] * 2
+in1 = [3, 2, 2, 2] * 2
+in2 = [4] * 2
+
+entry = 0
+in1i = [0, 0, 0]
+in2i = [0, 0]
+countdown = [None, None, None]
+deepi = 0
+
+newsize = []
+dataLength = 0
+
+while entry < numEntries:
+    if deepi != 0:
+        countdown[deepi - 1] -= 1
+
+    if deepi == 0:
+        deepi += 1
+        in1i[1] = in1i[0]
+        countdown[deepi - 1] = in1[in1i[1]]
+        print "in1[", in1i[1], "]", in1[in1i[1]], "(0)"
+        newsize.append(in1[in1i[1]])
+        in1i[1] += 1
+
+    elif deepi == 1:
+        deepi += 1
+        in1i[2] = in1i[1]
+        countdown[deepi - 1] = in1[in1i[2]]
+        print "in1[", in1i[2], "]", in1[in1i[2]], "(1)"
+        newsize.append(in1[in1i[2]])
+        in1i[2] += 1
+
+    elif deepi == 2:
+        deepi += 1
+        in2i[1] = in2i[0]
+        countdown[deepi - 1] = in2[in2i[1]]
+        print "in2[", in2i[1], "]", in2[in2i[1]], "(2)"
+        newsize.append(in2[in2i[1]])
+        in2i[1] += 1
+
+    elif deepi == 3:
+        dataLength += 1
+
+    while deepi != 0 and countdown[deepi - 1] == 0:
+        deepi -= 1
+        print "deepi", deepi
+        if deepi == 0:
+            in1i[0] = in1i[1]
+        elif deepi == 1:
+            in1i[1] = in1i[2]
+        elif deepi == 2:
+            pass
+
+    if deepi == 0:
+        entry += 1
+        in2i[0] = in2i[1]
