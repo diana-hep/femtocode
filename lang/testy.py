81b7a90ef40eee10f8da2aee0c4cb86fb14b5d48
Jim Pivarski <jpivarski@gmail.com>
2017-04-06 15:50:19 -0500

    exactly right

--- lang/testy.py
+++ lang/testy.py
@@ -1,8 +1,6 @@
 numEntries = 2
-data1 = [1, 2, 3, 4, 5, 6] * 2
-data2 = [1, 2, 3, 4] * 2
-in1 = [3, 2, 2, 2] * 2
-in2 = [4] * 2
+in1 = [3, 2, 2, 2, 3, 0, 1, 2]
+in2 = [4, 1]
 
 entry = 0
 in1i = [0, 0, 0]
@@ -21,23 +19,23 @@ while entry < numEntries:
         deepi += 1
         in1i[1] = in1i[0]
         countdown[deepi - 1] = in1[in1i[1]]
-        print "in1[", in1i[1], "]", in1[in1i[1]], "(0)"
+        print "in1[", in1i[1], "]", in1[in1i[1]]
         newsize.append(in1[in1i[1]])
         in1i[1] += 1
 
-    elif deepi == 1:
+    elif deepi == 2:
         deepi += 1
         in1i[2] = in1i[1]
         countdown[deepi - 1] = in1[in1i[2]]
-        print "in1[", in1i[2], "]", in1[in1i[2]], "(1)"
+        print "in1[", in1i[2], "]", in1[in1i[2]]
         newsize.append(in1[in1i[2]])
         in1i[2] += 1
 
-    elif deepi == 2:
+    elif deepi == 1:
         deepi += 1
         in2i[1] = in2i[0]
         countdown[deepi - 1] = in2[in2i[1]]
-        print "in2[", in2i[1], "]", in2[in2i[1]], "(2)"
+        print "in2[", in2i[1], "]", in2[in2i[1]]
         newsize.append(in2[in2i[1]])
         in2i[1] += 1
 
@@ -46,9 +44,9 @@ while entry < numEntries:
 
     while deepi != 0 and countdown[deepi - 1] == 0:
         deepi -= 1
-        print "deepi", deepi
         if deepi == 0:
             in1i[0] = in1i[1]
+            in2i[0] = in2i[1]
         elif deepi == 1:
             in1i[1] = in1i[2]
         elif deepi == 2:
@@ -56,4 +54,3 @@ while entry < numEntries:
 
     if deepi == 0:
         entry += 1
-        in2i[0] = in2i[1]
