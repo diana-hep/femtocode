c4d12d6fefc7c75e62b6e219a3a53a795edd8b1f
Jim Pivarski <jpivarski@gmail.com>
2017-04-11 23:49:45 -0500

    run/femtocode/run/execution.py retyped but completely untested

--- run/femtocode/run/execution.py
+++ run/femtocode/run/execution.py
@@ -155,14 +155,108 @@ class DeserializedLoopFunction(CompiledLoopFunction):
                 "parameters": [x.toJson() for x in self.parameters]}
 
 class NativeExecutor(Executor):
-    pass
+    def __init__(self, query, debug):
+        super(NativeExecutor, self).__init__(query, debug)
 
+    @staticmethod
+    def fromJson(obj):
+        assert isinstance(obj, dict)
+        assert obj["class"] == self.__class__.__module__ + "." + self.__class__.__name__
+        obj["class"] = "femtocode.execution.Executor"
+        out = Executor.fromJson(obj)
+        out.__class__ = NativeExecutor
+        return out
 
+    def compileLoops(self, debug):
+        fcntable = SymbolTable(StandardLibrary.table.asdict())
+        for lib in self.query.libs:
+            fcntable = fcntable.fork(lib.table.asdict())
 
+        for i, loop in enumerate(self.order):
+            if isinstance(loop, Loop):
+                fcnname = "f{0}_{1}".format(self.query.id, i)
+                loop.compileToPython(fcnname, self.query.inputs, fcntable, True, debug)
 
+                if loop.prerun is not None:
+                    fcn = compileToNative(loop.prerun, self.query.inputs)
+                    loop.prerun = CompiledLoopFunction(fcn, loop.prerun.parameters)
+
+                fcn = compileToNative(loop.run, self.query.inputs)
+                loop.run = CompiledLoopFunction(fcn, loop.run.parameters)
+
+    def makeArray(self, length, dataType, init):
+        if init:
+            return numpy.zeros(length, dtype=dataType)
+        else:
+            return numpy.empty(length, dtype=dataType)
 
 class NativeAsyncExecutor(NativeExecutor):
-    pass
+    def __init__(self, query, future, debug):
+        super(NativeAsyncExecutor, self).__init__(query, debug)
 
+        # all associated data are transient: they're lost if you serialize/deserialize
+        self.future = future
+        if self.future is not None:
+            self.loadsDone = dict((group.id, False) for group in query.dataset.groups)
+            self.computesDone = dict((group.id, False) for group in query.dataset.groups)
+            self.startTime = time.time()
+            self.computeTime = 0.0
 
+            self.action = self.query.actions[-1]
+            assert isinstance(self.action, statementlist.Aggregation), "last action must always be an aggregation"
+            self.tally = self.action.initialize()
 
+    def futureargs(self):
+        return (sum(1.0 for x in self.loadsDone.values() if x) / len(self.loadsDone),
+                sum(1.0 for x in self.computesDone.values() if x) / len(self.computesDone),
+                self.query.cancelled or all(self.computesDone.values()),
+                time.time() - self.startTime,
+                self.computeTime,
+                self.tally)
+
+    def oneLoadDone(self, groupid):
+        if self.future is not None:
+            with self.query.lock:
+                if not self.query.cancelled:
+                    self.loadsDone[groupid] = True
+                    futureargs = self.futureargs()
+
+                else:
+                    futureargs = None
+
+            if futureargs is not None:
+                self.future._update(*futureargs)
+
+    def oneComputeDone(self, groupid, computeTime, subtally):
+        if self.future is not None:
+            with self.query.lock:
+                if not self.query.cancelled:
+                    self.computesDone[groupid] = True
+                    self.computeTime += computeTime
+
+                    self.tally = self.action.update(self.tally, subtally)
+
+                    futureargs = self.futureargs()
+
+                else:
+                    futureargs = None
+
+            if futureargs is not None:
+                self.future._update(*futureargs)
+
+    def oneFailure(self, failure):
+        with self.query.lock:
+            self.query.cancelled = True
+            self.tally = failure
+            futureargs = self.futureargs()
+
+        self.future._update(*futureargs)
+
+    @staticmethod
+    def fromJson(obj):
+        assert isinstance(obj, dict)
+        assert obj["class"] == self.__class__.__module__ + "." + self.__class__.__name__
+        obj["class"] = "femtocode.execution.Executor"
+        out = Executor.fromJson(obj)
+        out.__class__ = NativeAsyncExecutor
+        return out
